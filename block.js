26f46a0571bbf1c32ba3e17af86e471d07b9af99
Sagyan Aryal <42304135+SagyanAryal@users.noreply.github.com>
2020-12-25 17:07:47 -0500

    V2

--- block.js
+++ block.js
@@ -1,24 +1,27 @@
-computeHash() {
-  return SHA256(JSON.stringify(this.data) + this.index + this.timestamp).toString();
+const {SHA256 } = require('crypto-js');
+
+class Block{
+  constructor(data, index, prevHash, timestamp){
+    this.index = index;
+    this.data = data;
+    this.hash = this.computeHash();
+    this.prevHash = prevHash;
+    this.timestamp = timestamp;
+
+  }
+
+
+  computeHash() {
+    return SHA256(JSON.stringify(this.data) + this.index + this.timestamp).toString();
+  }
 }
-}
+
+module.exports = Block
 
 let test = new Block(1, 1, 0, String(new Date()))
 let test2 = new Block(1, 1, 0, String(new Date()))
 let test3 = new Block(1, 2, 0, String(new Date()))
-getTimestamp() {
-  return this.timestamp;
-}
-}
 
-console.log(test.hash)
+console.log(test.timestamp)
 console.log(test2.hash)
 console.log(test3.hash)
-// let test = new Block(1, 1, 0, String(new Date()))
-// let test2 = new Block(1, 1, 0, String(new Date()))
-// let test3 = new Block(1, 2, 0, String(new Date()))
-//
-// console.log(test.hash)
-// console.log(test2.hash)
-// console.log(test3.hash)
-module.exports = Block
--- blockchain.js
+++ blockchain.js
@@ -1,26 +1,25 @@
-
-const Block = require('./block');
-
 import Block from './block.js'
 
-class Blockchain
 
 class Blockchain {
 
   constructor() {
     // this.chain = [this.GenesisBlock];
     this.chain = [this.GenesisBlock];
-
   }
 
   GenesisBlock(){
     return new Block(null, 0, null, String(new Date()))
   }
 
-  addBlock(data, index){
-    var prevHash = this.getCurrBlock().prevHash;
-    var block = new Block(data, index, prevHash, String(new Date()));
-    this.chain.push(block);
+  addBlock(newBlock){
+    newBlock.prevHash = this.getCurrBlock().hash;
+    newBlock.index = this.getCurrBlock().index + 1;
+    newBlock.hash = newBlock.computeHash();
+    newBlock.timestamp = String(new Date());
+    if(confirmValidity(newBlock)){
+      this.chain.push(newBlock);
+    }
   }
 
   getCurrBlock(){
@@ -28,11 +27,31 @@ class Blockchain {
   }
 
   // returns a boolean checks if a hash been compromised
-  confirmValidity (){
+  confirmValidity(newBlock){
 
+    for(int i = 1; i < this.chain.length; i++){
+      let curr = this.chain[i];
+      let prevBlock = this.chain[i-1];
+
+      if(curr.index !== prevBlock.index + 1){
+        return false;
+      }
+
+      if(curr.hash !== curr.computeHash()){
+        return false;
+      }
+
+      if (curr.prevHash !== prevBlock.hash) {
+        return false;
+      }
+    }
+    console.log("Valid block");
+    return true;
   }
-}
+
 
 let bChain = new Blockchain();
-bChain.addBlock(1, 1)
-console.log(bChain.getCurrBlock().getTimestamp())
+let newB = new Block(1, 1, 0, 3))
+bChain.addBlock(new Block(1, 1, 0, 3))
+console.log(bChain.getCurrBlock().timestamp)
+bChain.addBlock(new Block(1, 1, 0, 3))
